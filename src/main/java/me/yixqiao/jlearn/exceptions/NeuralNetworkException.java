185f00dd3669cfa54de8c4421487584b7ad917bb
Yixuan Qiao <yixqiao@gmail.com>
2020-05-27 09:28:18 -0700

    Add exception documentation
    Exception docs are fully completed

--- src/main/java/me/yixqiao/jlearn/exceptions/NeuralNetworkException.java
+++ src/main/java/me/yixqiao/jlearn/exceptions/NeuralNetworkException.java
@@ -1,5 +1,8 @@
 package me.yixqiao.jlearn.exceptions;
 
+/**
+ * Exception in neural network.
+ */
 public class NeuralNetworkException extends RuntimeException {
     public NeuralNetworkException() {
         super();
