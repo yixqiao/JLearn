2bb5f2585fc081f16698927ef7a0d125f8d9d97d
Yixuan Qiao <yixqiao@gmail.com>
2020-05-30 09:24:23 -0700

    Begin serialization

---
+++ src/main/java/me/yixqiao/jlearn/testing/SerializeTest.java
@@ -0,0 +1,11 @@
+package me.yixqiao.jlearn.testing;
+
+import me.yixqiao.jlearn.matrix.Matrix;
+
+public class SerializeTest {
+    public static void main(String[] args) {
+        Matrix m = new Matrix(new double[][]{{1}});
+        m.printMatrix();
+        
+    }
+}
