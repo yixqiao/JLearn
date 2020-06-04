8e010c7042b86814f9520327d42c758b9551b7d7
Yixuan Qiao <yixqiao@gmail.com>
2020-06-03 20:28:31 -0700

    Begin Population methods

--- src/main/java/me/yixqiao/jlearn/genetic/Individual.java
+++ src/main/java/me/yixqiao/jlearn/genetic/Individual.java
@@ -1,4 +1,25 @@
 package me.yixqiao.jlearn.genetic;
 
+import me.yixqiao.jlearn.layers.Layer;
+
+import java.util.ArrayList;
+
 public class Individual {
+    /**
+     * Layers.
+     */
+    protected final ArrayList<Layer> layers;
+
+    /**
+     * Number of layers (including input).
+     */
+    protected int layerCount;
+
+    public Individual() {
+        layers = new ArrayList<>();
+    }
+
+    public Individual(ArrayList<Layer> layers) {
+        this.layers = layers;
+    }
 }
--- src/main/java/me/yixqiao/jlearn/genetic/Population.java
+++ src/main/java/me/yixqiao/jlearn/genetic/Population.java
@@ -1,4 +1,46 @@
 package me.yixqiao.jlearn.genetic;
 
+import me.yixqiao.jlearn.exceptions.NeuralNetworkException;
+import me.yixqiao.jlearn.layers.InputLayer;
+import me.yixqiao.jlearn.layers.Layer;
+import me.yixqiao.jlearn.losses.Loss;
+import me.yixqiao.jlearn.models.Model;
+
+import java.util.ArrayList;
+
 public class Population {
+    protected final int indivCount;
+    protected ArrayList<Individual> individuals;
+
+    protected final ArrayList<Layer> layers;
+    protected int layerCount;
+
+    public Population(int indivCount) {
+        this.indivCount = indivCount;
+        individuals = new ArrayList<>();
+        layers = new ArrayList<>();
+    }
+
+
+    public Population addLayer(Layer layer) {
+        layers.add(layer);
+        return this;
+    }
+
+    public void prepLayers() {
+        layerCount = layers.size();
+        if (!(layers.get(0) instanceof InputLayer)) {
+            throw new NeuralNetworkException("First layer is not an input layer");
+        }
+        for (int layer = 0; layer < layerCount - 1; layer++) {
+            layers.get(layer + 1).initLayer(layers.get(layer).getOutSize(), layers.get(layer).getActivation());
+        }
+    }
+
+    public void initPop() {
+        individuals = new ArrayList<>();
+        for(int i=0; i<indivCount; i++){
+            individuals.add(new Individual(layers)); // Clone layers
+        }
+    }
 }
